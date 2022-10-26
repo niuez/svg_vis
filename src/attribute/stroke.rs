95ce131c1c030bd28be4ca72784c8f02f9b588ab
niuez <niuez.graph@gmail.com>
2022-10-26 13:11:00 +0900

    [add] stroke-opacity

--- src/attribute/stroke.rs
+++ src/attribute/stroke.rs
@@ -1,7 +1,11 @@
-use crate::literal::Color;
+use crate::literal::{
+    Color,
+    Percentage,
+};
 
 pub trait Stroke {
     fn stroke<C: Into<Color>>(self, color: C) -> Self;
+    fn stroke_opacity<P: Into<Percentage>>(self, per: P) -> Self;
 }
 
 macro_rules! implement_stroke {
@@ -9,6 +13,10 @@ macro_rules! implement_stroke {
         impl Stroke for $T {
             fn stroke<C: Into<Color>>(mut self, color: C) -> Self {
                 self.$svg = self.$svg.set("stroke", color.into());
+                self
+            }
+            fn stroke_opacity<P: Into<Percentage>>(mut self, per: P) -> Self {
+                self.$svg = self.$svg.set("stroke-opacity", per.into());
                 self
             }
         }

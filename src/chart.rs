3b7e509f7078726ba48796bada17e5b38ba825ff
niuez <niuez.graph@gmail.com>
2022-10-28 16:25:56 +0900

    feat(chart): absolute width and height of chart

--- src/chart.rs
+++ src/chart.rs
@@ -1,4 +1,5 @@
 use svg::Document;
+use svg::node::Value;
 use crate::attribute::{
     AbsPos,
 };
@@ -21,6 +22,14 @@ impl Chart {
     }
     pub fn scale(mut self, x: f32, y: f32) -> Self {
         self.scale = Scale::xy(x, y);
+        self
+    }
+    pub fn chart_width<V: Into<Value>>(mut self, w: V) -> Self {
+        self.svg = self.svg.set("width", w.into());
+        self
+    }
+    pub fn chart_height<V: Into<Value>>(mut self, h: V) -> Self {
+        self.svg = self.svg.set("height", h.into());
         self
     }
     pub fn draw<E: AbsPos, X: Into<Length>, Y: Into<Length>>(mut self, elem: E, x: X, y: Y) -> Self {
